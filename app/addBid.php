bf6c5c4512ce1560b4041bc9e2cf72e9858a6eb2
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2019-09-17 01:26:36 +0800

    Created addBid.php and Student.php
    Starting work on add bid functionality, created Student class to store bid related methods. Possibility of creating more methods to authenticate log in functionality as well

---
+++ app/addBid.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Place your bid!</title>
+</head>
+<body>
+<form>
+	
+</form>
+</body>
+</html>
---
+++ app/include/Student.php
@@ -0,0 +1,45 @@
+<?
+
+/**
+ * 
+ */
+class Student
+{
+	private $userid;
+	private $password;
+	private $name;
+	private $school;
+	private $edollar;
+
+	public function __construct($userid, $name, $school, $edollar)
+	{
+		$this->userid = $userid;
+		$this->name = $name;
+		$this->school = $school;
+		$this->edollar = $edollar;
+		
+	}
+
+	public function getEdollarAfterBid($edollarUsed)
+	{
+		$this->edollar -= $edollarUsed;
+		return $this->edollar;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getSchool()
+	{
+		return $this->school;
+	}
+
+	public function getEdollar()
+	{
+		return $this->edollar;
+	}
+}
+
+?>
