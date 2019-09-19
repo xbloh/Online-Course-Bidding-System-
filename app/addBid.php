28b81d040bfb91af9558b760069422ad0ea7c834
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2019-09-19 16:56:48 +0800

    Updated addBid.php
    for formatting of look and to let selected course stick

--- app/addBid.php
+++ app/addBid.php
@@ -4,16 +4,43 @@
 	<title>Place your bid!</title>
 </head>
 <body>
-	<form action="bidProcessing.php" method="post">
-		<select name='indexofCoursesToBid'>
-				<?php
-					require 'include/common.php';
-					foreach ($_SESSION['coursesAvailable'] as $index => $course) {
-						echo "<option value='$index'>" . $course->getCourseId() . " " . $course->getTitle() . "</option>";
-					}
-				?>
-		</select>
+	<form action="addBid.php" method="post">
+		<table>
+			<tr>
+				<th>
+					Select course to bid for:
+				</th>
+				<td>
+					<select name='indexOfCoursesToBid'>
+						<?php
+							require 'include/common.php';
+							foreach ($_SESSION['coursesAvailable'] as $index => $course) {
+								if (isset($_POST['indexOfCoursesToBid']) && $index == $_POST['indexOfCoursesToBid']) {
+									$selected = 'selected';
+
+								}else {
+									$selected = '';
+								}
+
+								echo "<option value='$index' $selected>" . $course->getCourseId() . " " . $course->getTitle() . "</option>";
+							}
+						?>
+					</select>
+				</td>
+				<td>
+					<input type="submit" name="courseSelected" value="Select">
+				</td>
+			</tr>
+		</table>
 	</form>
+
+	<?php
+
+		if (isset($_POST['courseSelected'])) {
+			# code...
+		}
+
+	?>
 
 </body>
 </html>
