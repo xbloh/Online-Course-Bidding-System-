1ca0b8252848ffd7bd9b37cb1e7d1f23c79163c9
triciatll <triciatan.2018@smu.edu.sg>
2019-09-20 13:27:03 +0800

    Update welcome.php
    Include user's eDollars and add bid function

--- app/welcome.php
+++ app/welcome.php
@@ -4,12 +4,17 @@
 require_once 'include/common.php';
 
 $student = $_SESSION['student'];
-$userid = $student->getUserId();
+$name = $student->getName();
+$eDollar = $student->getEdollar();
 
 var_dump($_SESSION);
 
-
 ?>
 
-Welcome to BOIS, <?php echo $userid; ?>
+Welcome to BOIS, <?php echo $name; ?>
+<br>
+You currently have <?php echo $eDollar; ?> eDollars
+<br>
+<a href ="../app/addBid.php">Click here to add bid</a>
+
 </html>
