336b9230e854ee16706aa5c7752395593d141736
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2019-10-16 00:19:17 +0800

    Create RoundDAO.php
    renamed from previous file

---
+++ app/include/RoundDAO.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once 'common.php';
+
+class RoundDAO {
+
+    public function startRound1()
+		{
+			$sql = 'UPDATE rounds SET round = 1, status = "active", WHERE round = 0;';
+        
+	        $connMgr = new ConnectionManager();      
+	        $conn = $connMgr->getConnection();
+            $stmt = $conn->prepare($sql);
+            
+            $status = FALSE;
+            if($stmt->execute())
+            {
+                $status = TRUE;
+            };
+            $stmt = null;
+            $pdo = null;
+            return $status;
+        }
+        
+        public function endRound1()
+		{
+			$sql = 'UPDATE rounds SET status = "completed", WHERE round = 1;';
+        
+	        $connMgr = new ConnectionManager();      
+	        $conn = $connMgr->getConnection();
+            $stmt = $conn->prepare($sql);
+            
+            $status = FALSE;
+            if($stmt->execute())
+            {
+                $status = TRUE;
+            };
+            $stmt = null;
+            $pdo = null;
+            return $status;
+        }
+        
+        public function startRound2()
+		{
+			$sql = 'UPDATE rounds SET round = 2, status = "active", WHERE round = 1;';
+        
+	        $connMgr = new ConnectionManager();      
+	        $conn = $connMgr->getConnection();
+            $stmt = $conn->prepare($sql);
+            
+            $status = FALSE;
+            if($stmt->execute())
+            {
+                $status = TRUE;
+            };
+            $stmt = null;
+            $pdo = null;
+            return $status;
+        }
+
+        public function endRound2()
+		{
+			$sql = 'UPDATE rounds SET status = "completed", WHERE round = 2;';
+        
+	        $connMgr = new ConnectionManager();      
+	        $conn = $connMgr->getConnection();
+            $stmt = $conn->prepare($sql);
+            
+            $status = FALSE;
+            if($stmt->execute())
+            {
+                $status = TRUE;
+            };
+            $stmt = null;
+            $pdo = null;
+            return $status;
+        }
+
+}
+
+
+?>
