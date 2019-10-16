d9e8b0e4fd9c0132c37206d0d1ba770462efab80
LOH Xiao Binn <xbloh.2018@sis.smu.edu.sg>
2019-10-16 11:13:42 +0800

    testing for JSON, standardising debugging output - error msg
    multiple test and standardising

--- app/json/authenticate.php
+++ app/json/authenticate.php
@@ -5,15 +5,15 @@
 
 
 	// isMissingOrEmpty(...) is in common.php
-	$errors = [ isMissingOrEmpty('username'), 
-	            isMissingOrEmpty('password') ];
+	$errors =  [isMissingOrEmpty('username'), 
+	            isMissingOrEmpty('password')];
 	$errors = array_filter($errors);
 
 
 	if (!isEmpty($errors)) {
 	    $result = [
 	        "status" => "error",
-	        "messages" => array_values($errors)];
+	        "message" => array_values($errors)];
 	        header('Content-Type: application/json');
 	        echo json_encode($result, JSON_PRETTY_PRINT);
 	        exit;
