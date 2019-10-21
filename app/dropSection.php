3052f3a5370df864db0606f90fc754ad43e8788e
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2019-10-21 17:26:41 +0800

    Update dropSection.php
    Create drop form

--- app/dropSection.php
+++ app/dropSection.php
@@ -5,6 +5,35 @@
 	$userId = $_SESSION['student']->getUserId();
 
 	$successfulBids = $bidDAO->retrieveUserSuccessfulBids($userId);
-	var_dump($successfulBids);
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Drop Section</title>
+</head>
+<body>
+	<h2>Select section to drop</h2>
+	<form action="dropSectionProcessing.php">
+		<table cellspacing="10px" cellpadding="3px">
+			<tr>
+				<th>Course Id</th>
+				<th>Section Id</th>
+				<th>Select to drop</th>
+			</tr>
+		
+		<?php
+			foreach ($successfulBids as $bid) {
+				$identifier = $bid['code'] . ',' . $bid['section'];
+				$courseId = $bid['code'];
+				$sectionId = $bid['section'];
+				echo "<tr><td>{$courseId}</td><td>{$sectionId}</td>
+				<td><input type='checkbox' name='toDrop[]' value={$identifier}></td></tr>";
+			}
+		?>
+		</table>
+		<input type="submit" name="Submit">
+	</form>
+	
+</body>
+</html>
