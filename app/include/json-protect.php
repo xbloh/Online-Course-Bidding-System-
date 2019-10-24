0ba8cdbe0419d6012e9a77e3dd8e29356277f8ff
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2019-10-24 13:40:58 +0800

    created json protection

---
+++ app/include/json-protect.php
@@ -0,0 +1,24 @@
+<?php
+	require_once 'token.php';
+
+	$errors = [];
+	
+	if (isset($_REQUEST['token'])) {
+		$token = $_REQUEST['token'];
+		if ($token == '') {
+			$errors[] = 'blank token';
+		} elseif (!verify_token($token)) {
+			$errors[] = 'invalid token';
+		}
+	} else {
+		$errors[] = 'missing token';
+	}
+
+	if (count($errors) > 0) {
+		$out = ["status" => "error" , "message" => $errors];
+		header('Content-Type: application/json');
+        echo json_encode($out, JSON_PRETTY_PRINT);
+        exit;
+	}
+
+?>
